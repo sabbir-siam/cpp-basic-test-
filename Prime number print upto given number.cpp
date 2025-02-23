06578089ade30e83f875de44443767dee5563ae2
sabbir-siam <sabbirsiam0103@gmail.com>
2025-02-23 20:59:06 +0600

    Add files via upload

---
+++ Prime number print upto given number.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+ Prime(int n) {
+    if (n <= 1) {
+        return false; 
+    }
+    
+    
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            return false; 
+        }
+    }
+    
+    return true; 
+}
+
+int main() {
+    int num;
+    cout << "Enter an integar number: ";
+    cin >> num;
+
+    if (Prime(num)) 
+        cout << num << " is a prime number." << endl;
+     else 
+        cout << num << " is not a prime number." << endl;
+    
+	cout << "Prime numbers up to " << num << ": ";
+    for (int i = 2; i <= num; i++) {
+        if (Prime(i)) 
+            cout << i << " ";
+        }
+    
+
+    return 0;
+}
+
